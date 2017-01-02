b4b63d6849c47eba50ba31fe4684027947c03ca2
David Leon Ortega <david_lagu@hotmail.com>
2017-01-02 13:59:10 +0000

    Primer commit
    Creado proyecto base.

---
+++ app/src/main/java/com/example/david/bvcontroller/BVControllerActivity.java
@@ -0,0 +1,13 @@
+package com.example.david.bvcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BVControllerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bvcontroller);
+    }
+}
