63f32339dce22237915d82f5a5ccfd921cfc8a0d
David Leon Ortega <david_lagu@hotmail.com>
2017-01-15 10:58:27 +0000

    Añadidas las acciones a botones.
    Mover arriba, abajo, izquierda y derecha.

--- app/src/main/java/com/example/david/bvcontroller/BVControllerActivity.java
+++ app/src/main/java/com/example/david/bvcontroller/BVControllerActivity.java
@@ -2,6 +2,10 @@ package com.example.david.bvcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class BVControllerActivity extends AppCompatActivity {
 
@@ -9,6 +13,43 @@ public class BVControllerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bvcontroller);
+        final Button buttonUp = (Button) findViewById(R.id.buttonUP);
+        final Button buttonBack = (Button) findViewById(R.id.buttonBack);
+        final Button buttonRight = (Button) findViewById(R.id.buttonRight);
+        final Button buttonLeft = (Button) findViewById(R.id.buttonLeft);
+
+        buttonUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                sendUpCommand();
+            }
+        });
+
+        buttonBack.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                sendBackCommand();
+            }
+        });
+
+        buttonRight.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                sendRightCommand();
+            }
+        });
+
+        buttonLeft.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                sendLeftCommand();
+            }
+        });
+
     }
 }
 
